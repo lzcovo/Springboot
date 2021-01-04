917dbad3cd3a7f5e538aad1f8b98c6ebf72c2ec7
Lu <lzcovo@gmail.com>
2021-01-04 18:05:26 +0800

    create project

---
+++ src/main/java/com/lzc/mybatis14/Mybatis14Application.java
@@ -0,0 +1,13 @@
+package com.lzc.mybatis14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatis14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzc/mybatis14/Mybatis14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzc.mybatis14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
